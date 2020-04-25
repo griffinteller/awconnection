0e067378f10bbd882685bff6bfcf2d14294e1227
griffinteller <griffinteller@gmail.com>
2020-04-24 18:02:15 -0700

    0.2.3! Coordinate flipping is now client side.

--- tests/MainTest.py
+++ tests/MainTest.py
@@ -1,19 +1,23 @@
 from awconnection import RobotConnection
 import matplotlib.pyplot as plt
 import time
+import copy
 
 r = RobotConnection()
 r.connect()
 
-r.set_tire_torque("BackLeft", 300)
-r.set_tire_torque("BackRight", 300)
+r.set_tire_torque("BackLeft", 5000)
+r.set_tire_torque("BackRight", 5000)
+r.set_tire_torque("FrontLeft", 5000)
+r.set_tire_torque("FrontRight", 5000)
+
+time.sleep(3)
 
 while True:
 
-    plt.pcolormesh(r.info.lidar.distance_matrix)
-    plt.gca().invert_yaxis()
-    plt.colorbar()
-    plt.show()
-    time.sleep(0.5)
+    r.set_tire_steering("BackLeft", 0)
+    r.set_tire_steering("BackRight", 0)
+    r.set_tire_steering("FrontLeft", 20)
+    r.set_tire_steering("FrontRight", 20)
 
 r.disconnect()
